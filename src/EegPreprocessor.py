09fcb31b53a09b1cd10f6f9080fe5f349ef12927
Jamil <jamilreza@iut-dhaka.edu>
2019-10-24 20:28:58 -0400

    Added finding and plotting EOG artifacts in Preprocessing file

--- src/EegPreprocessor.py
+++ src/EegPreprocessor.py
@@ -16,11 +16,11 @@ def load_montage(channel_names,
     return montage
 
 def load_biosemi_montage(biosemi_channel_count = 64):
-    montage = load_montage(kind = BIOSEMI_MACHINE_NAME + str(BIOSEMI_MACHINE_NAME))
+    montage = mne.channels.read_montage('biosemi64')
     return montage
 
 def load_raw_data(filepath, montage, eog = DEFAULT_EOG):
-    raw = mne.io.read_raw_edf(filepath, montage = montage, eog)
+    raw = mne.io.read_raw_edf(filepath, montage, eog)
     return raw
 
 def resample_raw_data(raw):
@@ -46,24 +46,56 @@ def get_average_eog(raw):
     average_eog = create_eog_epochs(raw).average()
     return average_eog
 
+def create_average_eog_epochs(raw):
+    reject = dict(eeg = 10e-6)
+    picks_eeg = mne.pick_types(raw.info, meg=False, eeg=True,eog=False,
+                               stim=False, exclude='bads')
+    eog_average = create_eog_epochs(raw, reject= dict(eeg =10e-6), picks =picks_eeg).average()
+    eog_epochs = create_eog_epochs(raw, reject = reject)#get single EOG trials
+    return eog_epochs, eog_average
+
+
+
 def apply_ICA(raw):
     raw.load_data()
 #1 Hz high pass filtering helpful for fitting ICA 
-    filtered_raw = raw.filter(1., None, n_jobs=1, fir_design='firwin')
-    picks_eeg = mne.pick_types(filtered_raw.info, meg=False, eeg=True,eog=False,stim=False, exclude='bads')
+    raw.filter(1., None, n_jobs=1, fir_design='firwin')
+    picks_eeg = mne.pick_types(raw.info, meg=False, eeg=True,eog=False,stim=False, exclude='bads')
 
-##Fit ICA: Application of ICA to find the EOG artifacts in the raw data
+#Fit ICA: Application of ICA to find the EOG artifacts in the raw data
     method='fastica'
-#
-##Choosing other parameters
+
+#Choosing other parameters
     n_components = 62 #if float then selection by explained varience of PCA
     decim = 3 #We need sufficient statistics not all time points time saving
-#
-##ICA non-deterministic so we generate random state for every time the code is run and we want same decomposition with same order of components
+
+#ICA non-deterministic so we generate random state for every time the code is run and we want same decomposition with same order of components
     random_state = 60
-##Defining ICA intance object
-    ica_with_artifacts = ICA(n_components=n_components,method=method,random_state=random_state)
-    
+
+#Defining ICA intance object
+    ica_with_artifacts = ICA(n_components = n_components, method = method, random_state = random_state)
     reject = dict(eeg = 10e-5)
-    ica = ica_with_artifacts.fit(filtered_raw,picks=picks_eeg,decim=decim,reject=reject)
-    return ica
+    ica = ica_with_artifacts.fit(raw, picks = picks_eeg, decim = decim, reject = reject)
+
+#Get single EOG trials
+    eog_epochs, eog_average = create_average_eog_epochs(raw)
+
+#Find via correlation
+    eog_inds, scores = ica.find_bads_eog(eog_epochs)
+    raw_copy = raw.copy().crop(0, 10)
+    ica.apply(raw_copy)
+
+    return ica, eog_inds, scores, eog_average, eog_epochs, raw_copy
+
+
+def find_eog_artifacts(raw, event_id):
+    eog_events = mne.preprocessing.find_eog_events(raw, event_id)
+        
+#read epochs
+    picks = mne.pick_types(raw.info, eeg = False, eog = True, 
+                               exclude = 'bads')
+    tmin, tmax = -0.2, 4.0
+    epochs = mne.Epochs(raw, eog_events, event_id, tmin, tmax, picks = picks)
+    data = epochs.get_data()
+    print("No. of detected EOG artifacts : %d" % len(data))
+    return epochs, data
