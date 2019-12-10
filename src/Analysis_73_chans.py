b4491ab84b5bc6e7301dc201861fa3d2d2a110ba
Jamil <jamilreza@iut-dhaka.edu>
2019-12-09 21:29:01 -0500

    Fixed the plotting of microstates maps issue: by Shiblu vai

--- src/Analysis_73_chans.py
+++ src/Analysis_73_chans.py
@@ -11,49 +11,134 @@ import microstates
 #import pca_ica_gfp_freq_bands as analysis gfp_analysis
 #import mara as mara1
 import scratch as testing
+import argparse, os, sys, time
 
+
+def read_edf(filename):
+    """Basic EDF file format reader
+
+    EDF specifications: http://www.edfplus.info/specs/edf.html
+
+    Args:
+        filename: full path to the '.edf' file
+    Returns:
+        chs: list of channel names
+        fs: sampling frequency in [Hz]
+        data: EEG data as numpy.array (samples x channels)
+    """
+
+    def readn(n):
+        """read n bytes."""
+        return np.fromfile(fp, sep='', dtype=np.int8, count=n)
+
+    def bytestr(bytes, i):
+        """convert byte array to string."""
+        return np.array([bytes[k] for k in range(i*8, (i+1)*8)]).tostring()
+
+    fp = open(filename, 'r')
+    x = np.fromfile(fp, sep='', dtype=np.uint8, count=256).tostring()
+    header = {}
+    header['version'] = x[0:8]
+    header['patientID'] = x[8:88]
+    header['recordingID'] = x[88:168]
+    header['startdate'] = x[168:176]
+    header['starttime'] = x[176:184]
+    header['length'] = int(x[184:192]) # header length (bytes)
+    header['reserved'] = x[192:236]
+    header['records'] = int(x[236:244]) # number of records
+    header['duration'] = float(x[244:252]) # duration of each record [sec]
+    header['channels'] = int(x[252:256]) # ns - number of signals
+    n_ch = header['channels']  # number of EEG channels
+    header['channelname'] = (readn(16*n_ch)).tostring()
+    header['transducer'] = (readn(80*n_ch)).tostring().split()
+    header['physdime'] = (readn(8*n_ch)).tostring().split()
+    header['physmin'] = []
+    b = readn(8*n_ch)
+    for i in range(n_ch): header['physmin'].append(float(bytestr(b, i)))
+    header['physmax'] = []
+    b = readn(8*n_ch)
+    for i in range(n_ch): header['physmax'].append(float(bytestr(b, i)))
+    header['digimin'] = []
+    b = readn(8*n_ch)
+    for i in range(n_ch): header['digimin'].append(int(bytestr(b, i)))
+    header['digimax'] = []
+    b = readn(8*n_ch)
+    for i in range(n_ch): header['digimax'].append(int(bytestr(b, i)))
+    header['prefilt'] = (readn(80*n_ch)).tostring().split()
+    header['samples_per_record'] = []
+    b = readn(8*n_ch)
+    for i in range(n_ch): header['samples_per_record'].append(float(bytestr(b, i)))
+    nr = header['records']
+    n_per_rec = int(header['samples_per_record'][0])
+    n_total = int(nr*n_per_rec*n_ch)
+    fp.seek(header['length'],os.SEEK_SET)  # header end = data start
+    data = np.fromfile(fp, sep='', dtype=np.int16, count=n_total)  # count=-1
+    fp.close()
+
+    # re-order
+    data = np.reshape(data,(n_per_rec,n_ch,nr),order='F')
+    data = np.transpose(data,(0,2,1))
+    data = np.reshape(data,(n_per_rec*nr,n_ch),order='F')
+
+    # convert to physical dimensions
+    for k in range(data.shape[1]):
+        d_min = float(header['digimin'][k])
+        d_max = float(header['digimax'][k])
+        p_min = float(header['physmin'][k])
+        p_max = float(header['physmax'][k])
+        if ((d_max-d_min) > 0):
+            data[:,k] = p_min+(data[:,k]-d_min)/(d_max-d_min)*(p_max-p_min)
+
+    print(header)
+    return header['channelname'].split(),\
+           header['samples_per_record'][0]/header['duration'],\
+           data
+channelData = read_edf('C:/projects/eeg_microstates/src/test.edf')
+data = channelData[2]
+n_maps = 4
+maps, L_, gfp_peaks, gev,cv = testing.kmeans(data, n_maps, n_runs = 5,maxerr = 1e-6, maxiter = 200 )
+
+
+#for i in range(1):
+#    print(__doc__)
     
-for i in range(1):
-    print(__doc__)
+    
+#    DEFAULT_EOG = ['EXG1', 'EXG2', 'EXG3', 'EXG4', 'EXG5', 'EXG6', 'EXG7', 'EXG8']
+    
+#    def get_raw_data_file_path():
+#        #root = tk.Tk()
+#        #root.withdraw()
+#        #data_file_path  = filedialog.askopenfilename()
+#        return 'C:/projects/eeg_microstates/src/test.edf'
+    
+#    #Channel position function for Biosemi machine
+#    def load_biosemi_montage(string = 'biosemi64'):
+#        montage = mne.channels.make_standard_montage(string)
+#        return montage
+    
+#    #Loading the raw data
+#    def load_raw_data(filepath, montage, eog = DEFAULT_EOG):
+#        raw = mne.io.read_raw_edf(filepath, montage, eog, preload =True)
+#        return raw
     
     
-    DEFAULT_EOG = ['EXG1', 'EXG2', 'EXG3', 'EXG4', 'EXG5', 'EXG6', 'EXG7', 'EXG8']
-    
-    def get_raw_data_file_path():
-        root = tk.Tk()
-        root.withdraw()
-        data_file_path  = filedialog.askopenfilename()
-        return data_file_path
-    
-    #Channel position function for Biosemi machine
-    def load_biosemi_montage(string = 'biosemi64'):
-        montage = mne.channels.make_standard_montage(string)
-        return montage
-    
-    #Loading the raw data
-    def load_raw_data(filepath, montage, eog = DEFAULT_EOG):
-        raw = mne.io.read_raw_bdf(filepath, montage, eog, preload =True)
-        return raw
-    
-    
-    def preprocess_raw_data():
-        print("Please enter the raw data file")
-        filepath = get_raw_data_file_path()
+#    def preprocess_raw_data():
+#        print("Please enter the raw data file")
+#        filepath = get_raw_data_file_path()
         
-        montage = load_biosemi_montage()
-        raw = load_raw_data(filepath, montage)
+#        montage = load_biosemi_montage()
+#        raw = load_raw_data(filepath, montage)
         
-    #Using the average EEG reference
-        raw.set_eeg_reference('average')
-    #High pass filtering of data
-        raw.filter(0.2, None)
-        return raw
-    raw = preprocess_raw_data()
+#    #Using the average EEG reference
+#        raw.set_eeg_reference('average')
+#    #High pass filtering of data
+#        raw.filter(0.2, None)
+#        return raw
+#    raw = preprocess_raw_data()
     
-    data = raw.get_data()
-    #data = np.resize(data,(30,))
-    n_maps = 4
-    #maps, L_, gfp_peaks, gev,cv = testing.kmeans(data, n_maps, n_runs = 5,maxerr = 1e-6, maxiter = 200 )
+#    data = raw.get_data()
+#    #data = np.resize(data,(30,))
+
     
     
     
--- src/scratch.py
+++ src/scratch.py
@@ -1,4 +1,41 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import cmath
+from scipy.interpolate import griddata
+
+def read_xyz(filename):
+    """Read EEG electrode locations in xyz format
+
+    Args:
+        filename: full path to the '.xyz' file
+    Returns:
+        locs: n_channels x 3 (numpy.array)
+    """
+    ch_names = []
+    locs = []
+    with open(filename, 'r') as f:
+        l = f.readline()  # header line
+        while l:
+            l = f.readline().strip().split("\t")
+            if (l != ['']):
+                ch_names.append(l[0])
+                locs.append([float(l[1]), float(l[2]), float(l[3])])
+            else:
+                l = None
+    return ch_names, np.array(locs)
+
+def findstr(s, L):
+    """Find string in list of strings, returns indices.
+
+    Args:
+        s: query string
+        L: list of strings to search
+    Returns:
+        x: list of indices where s is found in L
+    """
+
+    x = [i for i, l in enumerate(L) if (l==s)]
+    return x
 
 
 def locmax(x):
@@ -18,8 +55,62 @@ def locmax(x):
 
 
 
+def topo(data, n_grid=64):
+    """Interpolate EEG topography onto a regularly spaced grid
 
-def kmeans(data, n_maps, n_runs = 10, maxerr = 1e-6, maxiter =500):
+    Args:
+        data: numpy.array, size = number of EEG channels
+        n_grid: integer, interpolate to n_grid x n_grid array, default=64
+    Returns:
+        data_interpol: cubic interpolation of EEG topography, n_grid x n_grid
+                       contains nan values
+    """
+    channels, locs = read_xyz('cap.xyz')
+    n_channels = len(channels)
+    #locs /= np.sqrt(np.sum(locs**2,axis=1))[:,np.newaxis]
+    locs /= np.linalg.norm(locs, 2, axis=1, keepdims=True)
+    c = findstr('Cz', channels)[0]
+    # print 'center electrode for interpolation: ' + channels[c]
+    #w = np.sqrt(np.sum((locs-locs[c])**2, axis=1))
+    w = np.linalg.norm(locs - locs[c], 2, axis=1)
+    #arclen = 2*np.arcsin(w/2)
+    arclen = np.arcsin(w / 2. * np.sqrt(4. - w * w))
+    
+    it1 = locs[:,0] - locs[c][0]
+    it2 = locs[:,1] - locs[c][1]
+    mapped = map(complex, it1, it2)
+    listMapped = list(mapped)
+    phi = np.angle(listMapped)
+    X = arclen * np.real(np.exp(1j * phi))
+    Y = arclen * np.imag(np.exp(1j * phi))
+    r = max([max(X),max(Y)])
+    Xi = np.linspace(-r,r,n_grid)
+    Yi = np.linspace(-r,r,n_grid)
+    data_ip = griddata((X, Y), data, (Xi[None,:], Yi[:,None]), method='cubic')
+    return data_ip
+
+
+
+def eeg2map(data):
+    """Interpolate and normalize EEG topography, ignoring nan values
+
+    Args:
+        data: numpy.array, size = number of EEG channels
+        n_grid: interger, interpolate to n_grid x n_grid array, default=64
+    Returns:
+        top_norm: normalized topography, n_grid x n_grid
+    """
+    n_grid = 64
+    top = topo(data, n_grid)
+    mn = np.nanmin(top)
+    mx = np.nanmax(top)
+    top_norm = (top-mn)/(mx-mn)
+    return top_norm
+
+
+
+
+def kmeans(data, n_maps, n_runs=10, maxerr=1e-6, maxiter=500, doplot=True):
 #Arguments
 #data is numpy array size = no of EEG channels
 #n_maps is no of microstate maps
@@ -28,7 +119,7 @@ def kmeans(data, n_maps, n_runs = 10, maxerr = 1e-6, maxiter =500):
 #maxiter: maximum number of iterations (optional)
 #doplot is plot results, default false(optional)
 #returns:
-# maps: microstate map (no. of maps x no. of channels)
+# maps: microstate map (no.  of maps x no.  of channels)
 #L: sequence of microstate labesls
 #gfp_peaks: indices of local GFP maxima
 #gev: global explained varience (0..1)
@@ -60,13 +151,13 @@ def kmeans(data, n_maps, n_runs = 10, maxerr = 1e-6, maxiter =500):
         rndi = np.random.permutation(n_gfp)[:n_maps]
         maps = V[rndi, :]
         #normalize row -wise (across EEG channels)
-        maps /= np.sqrt(np.sum(maps **2, axis=1, keepdims=True))
+        maps /= np.sqrt(np.sum(maps ** 2, axis=1, keepdims=True))
         #initialize
         n_iter = 0
         var0 = 1.0
         var1 = 0.0
         #convergence criterion: varience estimate (step 6)
-        while((np.abs((var0-var1)/var0)> maxerr) & (n_iter < maxiter) ):
+        while((np.abs((var0 - var1) / var0) > maxerr) & (n_iter < maxiter)):
             #(step 3) microstate sequence (= current cluster assigment)
 
             C = np.dot(V, maps.T)
@@ -75,20 +166,20 @@ def kmeans(data, n_maps, n_runs = 10, maxerr = 1e-6, maxiter =500):
 
             #(step 4)
             for k in range(n_maps):
-                 Vt = V[L==k, :]
+                 Vt = V[L == k, :]
                  #Step 4a
                  Sk = np.dot(Vt.T, Vt)
                  #step 4b
                  evals, evecs = np.linalg.eig(Sk)
                  v = evecs[:, np.argmax(np.abs(evals))]
-                 maps[k,:] = v/np.sqrt(np.sum(v **2))
+                 maps[k,:] = v / np.sqrt(np.sum(v ** 2))
             #Step 5
             var1 = var0
             var0 = sumV2 - np.sum(np.sum(maps[L,:] * V, axis=1) ** 2)
-            var0 /= (n_gfp * (n_channels-1))
+            var0 /= (n_gfp * (n_channels - 1))
             n_iter += 1
-            if(n_iter <maxiter):
-                print("\t\tK-means run {:d}/{:d} converged after {:d} iterations." .format(run+1, n_runs, n_iter))
+            if(n_iter < maxiter):
+                print("\t\tK-means run {:d}/{:d} converged after {:d} iterations." .format(run + 1, n_runs, n_iter))
             else:
                 print("\t\tK-means run {:d}/{:d} did NOT converge after {:d} iterations.".format(run + 1, n_runs, maxiter))
 
@@ -96,16 +187,16 @@ def kmeans(data, n_maps, n_runs = 10, maxerr = 1e-6, maxiter =500):
             #Cross-validation criterion for this run (step 8)
             C_ = np.dot(data, maps.T)
             C_ /= (n_channels * np.outer(gfp, np.std(maps, axis=1)))
-            L_ = np.argmax(C_**2, axis=1)
-            var = np.sum(data**2) - np.sum(np.sum(maps[L_,:]*data, axis=1)**2)
-            var /= (n_time_samples*(n_channels-1))
-            cv = var * (n_channels-1) ** 2/(n_channels-n_maps-1.)**2
+            L_ = np.argmax(C_ ** 2, axis=1)
+            var = np.sum(data ** 2) - np.sum(np.sum(maps[L_,:] * data, axis=1) ** 2)
+            var /= (n_time_samples * (n_channels - 1))
+            cv = var * (n_channels - 1) ** 2 / (n_channels - n_maps - 1.) ** 2
 
             #GEV of cluster k
             gev = np.zeros(n_maps)
             for k in range(n_maps):
-                r =(L==k)
-                gev[k] = np.sum(gfp_values[r] ** 2 * C[r,k] **2)/gfp2
+                r = (L == k)
+                gev[k] = np.sum(gfp_values[r] ** 2 * C[r,k] ** 2) / gfp2
             gev_total = np.sum(gev)
 
             #Store
@@ -122,6 +213,59 @@ def kmeans(data, n_maps, n_runs = 10, maxerr = 1e-6, maxiter =500):
             #ms_gfp = ms_list[k_opt] # microstate sequence at GFP peaks
             gev = gev_list[k_opt]
             L_ = L_list[k_opt]
+            
+            if doplot:
+                plt.ion()
+                # matplotlib's perceptually uniform sequential colormaps:
+                # magma, infermo, plasma, viridis
+                cm = plt.cm.magma
+                fig, axarr = plt.subplots(1, n_maps, figsize=(20,5))
+                fig.patch.set_facecolor('white')
+                for imap in range(n_maps):
+                    axarr[imap].imshow(eeg2map(maps[imap, :]), cmap=cm, origin ='lower')
+                    axarr[imap].set_xticks([])
+                    axarr[imap].set_xticklabels([])
+                    axarr[imap].set_yticks([])
+                    axarr[imap].set_yticklabels([])
+                title = "K-means cluster centroids"
+                axarr[0].set_title(title, fontsize=20, fontweight="bold")
+                plt.show()
+
+                # We assign map labels manually
+                order_str = input("\n\t\tAssign map labels (e.g 0, 2, 1, 3);")
+                order_str = order_str.replace(",","")
+                order_str = order_str.replace(" ", "")
+                if (len(order_str) != n_maps):
+                    if (len(order_str) == 0):
+                        print("\t\tEmpty input string.")
+                    else:
+                        print("\t\tParsed manual input: {:s}".format(",".join(order_str)))
+                        print("\t\tNumber of labels does not equal number of clusters.")
+                    print("\t\t\Continue using the original assignment...\n")
+                else:
+                    order = np.zeros(n_maps, dtype = int)
+                    for i, s in enumerate(order_str):
+                        order[i] = int(s)
+                    print("\t\tRe-ordered labels: {:s}".format(", ".join(order_str)))
+                    #re-ordering return varaibles
+                    maps = maps[order,:]
+                    for i in range(len(L)):
+                        L[i] = order[L[i]]
+                    gev = gev[order]
+                    #Figure
+                    fig, axarr = plt.subplots(1, n_maps, figsize =(20,5))
+                    fig.patch.set_facecolor('white')
+                    for imap in range(n_maps):
+                        axarr[imap].imshow(eeg2map(maps[imap, :]), cmap=cm, origin='lower')
+                        axarr[imap].set_xticks([])
+                        axarr[imap].set_xticklabels([])
+                        axarr[imap].set_yticks([])
+                        axarr[imap].set_yticklabels([])
+                    title = "reordered K-means cluster centroids"
+                    axarr[0].set_title(title, fontsize = 20, fontweight= "bold")
+                    plt.show()
+                    plt.ioff()
+
     return maps, L_, gfp_peaks, gev,cv
 
 
